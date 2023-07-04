c5f27c296e6c2fbfc939c5c2fe25ac2141036f57
RohanAcharV <rohanacharv@gmail.com>
2023-07-04 17:00:20 +0530

    changes done

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link ,useLocation} from 'react-router-dom';
 function Nav(){
+  const location = useLocation();
 return(
     <div id="navigation">
     <ul>
         <li>
-          <Link to="/">ABOUT</Link>
+          <Link to="/" className={location.pathname === '/' ? 'active' : ''}>ABOUT</Link>
         </li>
         <li>
-          <Link to="/skills">SKILLS</Link>
+          <Link to="/skills" className={location.pathname === '/skills' ? 'active' : ''}>SKILLS</Link>
         </li>
         <li>
-          <Link to="/projects">PROJECTS</Link>
+          <Link to="/projects" className={location.pathname === '/projects' ? 'active' : ''}>PROJECTS</Link>
         </li>
       </ul>
     </div>
